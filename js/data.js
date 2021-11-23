bc489a64723979d40255119f6424b33d801c3227
Alessandro <luisialessandro95@gmail.com>
2021-11-23 19:17:24 +0100

    init js

--- js/data.js
+++ js/data.js
@@ -10,120 +10,120 @@
 // BONUS 1
 // Modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F. 2- popolare le options della select della milestone 3 dinamicamente.
 
-// [
-// 	{
-// 		name: 'cat',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'crow',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'dog',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'dove',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'dragon',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'horse',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'hippo',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'fish',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-// 	{
-// 		name: 'carrot',
-// 		prefix: 'fa-',
-// 		type: 'vegetable',
-// 		family: 'fas',
-// 		color: 'green'
-// 	},
-// 	{
-// 		name: 'apple-alt',
-// 		prefix: 'fa-',
-// 		type: 'vegetable',
-// 		family: 'fas',
-// 		color: 'green'
-// 	},
-// 	{
-// 		name: 'lemon',
-// 		prefix: 'fa-',
-// 		type: 'vegetable',
-// 		family: 'fas',
-// 		color: 'green'
-// 	},
-// 	{
-// 		name: 'pepper-hot',
-// 		prefix: 'fa-',
-// 		type: 'vegetable',
-// 		family: 'fas',
-// 		color: 'green'
-// 	},
-// 	{
-// 		name: 'user-astronaut',
-// 		prefix: 'fa-',
-// 		type: 'user',
-// 		family: 'fas',
-// 		color: 'blue'
-// 	},
-// 	{
-// 		name: 'user-graduate',
-// 		prefix: 'fa-',
-// 		type: 'user',
-// 		family: 'fas',
-// 		color: 'blue'
-// 	},
-// 	{
-// 		name: 'user-ninja',
-// 		prefix: 'fa-',
-// 		type: 'user',
-// 		family: 'fas',
-// 		color: 'blue'
-// 	},
-// 	{
-// 		name: 'user-secret',
-// 		prefix: 'fa-',
-// 		type: 'user',
-// 		family: 'fas',
-// 		color: 'blue'
-// 	}
-// ];
+const arrayIcons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
 
 
 
@@ -135,3 +135,24 @@
 // });
 
 // console.log(numberTre);
+
+
+let items = '';
+
+const container = document.querySelector('.container');
+
+for (let i = 0; i < arrayIcons.length; i++){
+    // console.log(arrayIcons[i].name);
+    items +=`
+        <div class="box-icon">
+            <div class="icon">
+                <i class="fas fa-cat"></i>
+            </div>
+            <div class="text">
+                <span>Cat</span>
+            </div>
+        </div>
+    `
+    console.log(items);
+    container.innerHTML = items;
+}
